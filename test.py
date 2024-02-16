83915766e922d78f561133ee2ca62c11f5ebf4f7
Jan Horacek <me@apophis.cz>
2024-02-16 20:53:55 +0100

    Add cipher.py & test.py.

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import cipher
+
+
+def test_duplicate() -> None:
+    # TODO
+    # assert cipher.duplicate('') == ''
+    pass
+
+
+def test_intersection() -> None:
+    # TODO
+    pass
+
+
+def test_caesar() -> None:
+    # TODO
+    pass
+
+
+def main() -> None:
+    test_duplicate()
+    test_intersection()
+    test_caesar()
+
+
+if __name__ == '__main__':
+    main()
